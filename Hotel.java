d60c96c6a0d200db436bfb27ad7cf227d14f3236
abdulwasayrida <64105216+abdulwasayrida@users.noreply.github.com>
2020-06-22 21:01:20 -0700

    Javadoc Comment (Hotel)

--- Hotel.java
+++ Hotel.java
@@ -1,3 +1,17 @@
+/**
+ * Hotel.java
+ * 
+ * @author Rida Abdulwasay – Pro Team Coordinator
+ * @author Ameya Dighe – Screen Output Expert
+ * @author Alvin Fabrio – Hash Table Whiz
+ * @author Daozhen (Leslie) Liu – BST Algorithms Specialist
+ * @author Samantha Tancio – Presentation Guru
+ * @author Vinh Vu – File I/O Ace
+ * 
+ * CIS 22C Project
+ * 
+ */
+
 public class Hotel implements Comparable<Hotel> {
 	private String roomNumber; // unique Key
 	private String occupants; // secondary Key
